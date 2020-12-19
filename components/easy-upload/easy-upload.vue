8734af333a53eecb828e5271c044b1ade543b0b3
GooBad <39213735+SevenCold@users.noreply.github.com>
2020-12-19 22:17:41 +0800

    commit code

---
+++ components/easy-upload/easy-upload.vue
@@ -0,0 +1,286 @@
+<template>
+	<view class="upload">
+		<block v-for="(upload,index) in uploads" :key="index">
+			<view class="uplode-file">
+				<image v-if="types == 'image'" class="uploade-img" :src="upload" :data-src="upload" @tap="previewImage"></image>
+				<!-- <image v-if="types == 'image'" class="clear-one-icon" :src="clearIcon" @tap="delImage(index)"></image> -->
+				<u-icon name="close" size="40" class="clear-one-icon" @click="delImage(index)"></u-icon>
+				<video v-if="types == 'video'" class="uploade-img" :src="upload" controls>
+					<cover-image v-if="types == 'video'" class="clear-one-icon" :src="clearIcon" @tap="delImage(index)"></cover-image>
+				</video>
+			</view>
+		</block>
+		<view v-if="uploads.length < uploadCount" :class="uploadIcon ? 'uploader-icon' : 'uploader-input-box'">
+			<view v-if="!uploadIcon" class="uploader-input" @tap="chooseUploads"></view>
+			<image v-else class="image-cion" :src="uploadIcon" @tap="chooseUploads"></image>
+		</view>
+	</view>	
+</template>
+
+<script>
+	export default{
+		props: {
+			types: {
+				type: String,
+				default: 'image'
+			},
+			dataList: {
+				type: Array,
+				default: function() {
+					return []
+				}
+			},
+			clearIcon: {
+				type: String,
+				default: 'http://img1.imgtn.bdimg.com/it/u=451604666,2295832001&fm=26&gp=0.jpg'
+			},
+			uploadIcon: {
+				type: String,
+				default: ''
+			},
+			uploadUrl: {
+				type: String,
+				default: ''
+			},
+			deleteUrl: {
+				type: String,
+				default: ''
+			},
+			uploadCount: {
+				type: Number,
+				default: 1
+			},
+			//上传图片大小 默认3M
+			upload_max: {
+				type: Number,
+				default: 3
+			}
+		},
+		data(){
+			return {
+				//上传的图片地址
+				uploadImages: [],
+				//展示的图片地址
+				uploads: [],
+				// 超出限制数组
+				exceeded_list: [],
+			}
+		},
+		watch:{
+			dataList:{
+				handler(val){
+					this.uploads = val;
+				},
+				immediate: true
+			}
+		},
+		methods:{
+			previewImage (e) {
+				var current = e.target.dataset.src
+				uni.previewImage({
+					current: current,
+					urls: this.dataList
+				})
+			},
+			chooseUploads(){
+				switch (this.types){
+					case 'image': 
+						uni.chooseImage({
+							count: this.uploadCount - this.uploads.length, //默认9
+							sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+							sourceType: ['album', 'camera'], //从相册选择
+							success: (res) => {
+								for(let i = 0; i< res.tempFiles.length; i++){
+									if(Math.ceil(res.tempFiles[i].size / 1024) < this.upload_max * 1024){
+										this.uploads.push(res.tempFiles[i].path)
+										this.uploadImages.push(res.tempFiles[i].path)
+									}else {
+										this.exceeded_list.push(i === 0 ? 1 : i + 1);
+										uni.showModal({
+											title: '提示',
+											content: `第${[...new Set(this.exceeded_list)].join(',')}张图片超出限制${this.upload_max}MB,已过滤`
+										});
+									}
+								}
+								this.$emit('successImageChoose', res.tempFiles)
+							},
+							fail: (err) => {
+							}
+						});
+					break;
+					case 'video' :
+						uni.chooseVideo({
+							sourceType: ['camera', 'album'],
+							success: (res) => {
+								if(Math.ceil(res.size / 1024) < this.upload_max * 1024){
+									this.uploads.push(res.tempFilePath)
+									uni.uploadFile({
+										url: this.uploadUrl, //仅为示例，非真实的接口地址
+										filePath: res.tempFilePath,
+										name: 'file',
+										//请求参数
+										formData: {
+											'user': 'test'
+										},
+										success: (uploadFileRes) => {
+											this.$emit('successVideo',uploadFileRes)
+										}
+									});
+								}else {
+									uni.showModal({
+										title: '提示',
+										content: `第${[...new Set(this.exceeded_list)].join(',')}张视频超出限制${this.upload_max}MB,已过滤`
+									});
+								}
+							},
+							fail: (err) => {
+								uni.showModal({
+									content: JSON.stringify(err)
+								});
+							}
+						});
+					break;
+				}
+			},
+			delImage(index){
+				//第一个是判断app或者h5的 第二个是判断小程序的
+				if(this.uploads[index].substring(0,4) !== 'http' || this.uploads[index].substring(0,11) == 'http://tmp/'){
+					this.uploads.splice(index,1)
+					this.$emit('successDelImage')
+					return;
+				};
+				// this.uploads.splice(index,1)
+				
+				// uni.showToast({
+				// 	title: '删除成功'
+				// })
+				// if(!this.deleteUrl) {
+				// 	uni.showModal({
+				// 		content: '请填写删除接口'
+				// 	});
+				// 	return;
+				// };
+				// uni.request({
+				// 	url: this.deleteUrl,
+				// 	method: 'DELETE',
+				// 	data: {
+				// 		image: this.dataList[index]
+				// 	},
+				// 	success: res => {
+				// 		if(res.data.status == 1) {
+				// 			uni.showToast({
+				// 				title: '删除成功'
+				// 			})
+				// 			this.uploads.splice(index,1)
+				// 		}
+				// 	},
+				// });
+			},
+			upload(){
+				if(!this.uploadUrl) {
+					uni.showModal({
+						content: '请填写上传接口'
+					});
+					return;
+				};
+				for (let i of this.uploadImages) {
+					uni.uploadFile({
+						url: this.uploadUrl, //仅为示例，非真实的接口地址
+						filePath: i,
+						name: 'file',
+						//请求参数
+						formData: {
+							'user': 'test'
+						},
+						success: (uploadFileRes) => {
+							this.$emit('successImage',uploadFileRes)
+						}
+					});
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.upload {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
+	.uplode-file {
+		margin: 10upx;
+		width: 550upx;
+		height: 300upx;
+		position: relative;
+	}
+	.uploade-img {
+		display: block;
+		width: 550upx;
+		height: 300upx;
+	}
+	.clear-one{
+		position: absolute;
+		top: -10rpx;
+		right: 0;
+	}
+	.clear-one-icon{
+		position: absolute;
+		width: 20px;
+		height: 20px;
+		top: 0;
+		right: 0;
+		z-index: 9;
+	}
+	.uploader-input-box {
+		position: relative;
+		margin:10upx;
+		width: 550upx;
+		height: 300upx;
+		border: 2upx solid #D9D9D9;
+	}
+	.uploader-input-box:before,
+	.uploader-input-box:after {
+		content: " ";
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		-webkit-transform: translate(-50%, -50%);
+		transform: translate(-50%, -50%);
+		background-color: #D9D9D9;
+	}
+	.uploader-input-box:before {
+		width: 4upx;
+		height: 79upx;
+	}
+	.uploader-input-box:after {
+		width: 79upx;
+		height: 4upx;
+	}
+	.uploader-input-box:active {
+		border-color: #999999;
+	}
+	.uploader-input-box:active:before,
+	.uploader-input-box:active:after {
+		background-color: #999999;
+	}
+	.uploader-input {
+		position: absolute;
+		z-index: 1;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		opacity: 0;
+	}
+	.uploader-icon{
+		position: relative;
+		margin:10upx;
+		width: 208upx;
+		height: 208upx;
+	}
+	.uploader-icon .image-cion{
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,149 @@
+<template>
+	<view class="page">
+		<u-card title="添加图片" title-size="40" border-radius="0">
+			<view class="" slot="body" :prop="need_init" :change:prop="ocr.initWorker">
+				<view class="u-body-item u-flex u-col-between u-p-t-0" :prop="base64" :change:prop="ocr.receiveImg">
+					<easy-upload
+					:dataList="imageList" @successDelImage="successDelImage" @successImageChoose="successImageChoose"></easy-upload>
+				</view>
+			</view>
+			<view class="" slot="foot" v-if="rec_status !== 'no'">
+				<u-line-progress v-if="rec_status === 'doing'" :percent="process" type="success" :height="100" :show-percent="true">
+					<view>识别中: {{process + '%'}}</view>
+				</u-line-progress>
+				<view class="content" v-if="rec_status === 'finished'">
+					<view v-for="(content in ocr_contents">{{content}}</view>
+				</view>
+			</view>
+		</u-card>
+	</view>
+</template>
+<script>
+	import easyUpload from '@/components/easy-upload/easy-upload.vue'
+	export default {
+		data() {
+			return {
+				imageList: [],
+				base64: '',
+				process: 0,
+				rec_status: 'no',    // 识别图像状态： no、doing、finished
+				ocr_contents: '',
+				need_init: false    // 加载worker
+			}
+		},
+		mounted() {
+			// 创建时，改变该变量，触发加载worker事件
+			this.need_init = !this.need_init
+		},
+		components:{
+			easyUpload
+		},
+		methods: {
+			getProcess(worker) {
+				if (worker.status === 'recognizing text') {
+					// 加强显示效果，因为识别总是从0.6 -> 1
+					if (worker.progress < 0.7) {
+						this.process = parseInt(worker.progress * 100) + 25
+					} else {
+						this.process = parseInt(worker.progress * 100)
+					}
+				}
+			},
+			// 接收识别结果，并赋值
+			setText (text) {
+				let lines = text.split('\n')
+				this.ocr_contents = lines
+				// 清空base64的值，避免识别同一图片，触发不了更新
+				this.base64 = ''
+				// 识别结束
+				this.rec_status = 'finished'
+				this.process = 0
+			},
+			successDelImage() {
+				// 删除图片时，状态为no，都不显示
+				this.rec_status = 'no'
+			},
+			successImageChoose() {
+				let localPath = this.imageList[0] 
+				let _this = this
+				// 状态改为doing
+				this.rec_status = 'doing'
+				uni.compressImage({
+					src: localPath,
+					quality: 80,
+					success: ys => {
+						plus.io.resolveLocalFileSystemURL(ys.tempFilePath, function(entry) {
+							entry.file(function(file) {
+								var fileReader = new plus.io.FileReader();
+								fileReader.readAsDataURL(file); 
+								fileReader.onloadend = function(evt) {
+									_this.base64 = evt.target.result
+								}
+							});
+						});
+					}
+				})
+			},
+		}
+	}
+</script>
+
+<script module="ocr" lang="renderjs">
+	import Tesseract from 'static/ocr/tesseract.min.js'
+	let worker
+	export default {
+		mounted: function() {
+		},
+		methods: {
+			initWorker: async function(newValue, oldValue, ownerInstance, instance) {
+				if (worker == undefined) {
+					const { createWorker } = Tesseract;
+					const localPath = plus.io.convertLocalFileSystemURL('_downloads/')
+					try{
+						worker = createWorker({
+							workerPath: localPath + '/worker.min.js',
+							langPath: localPath,
+							corePath: localPath +  'tesseract-core.wasm.js',
+							logger: function(m) {
+								ownerInstance.callMethod('getProcess', m)
+							},
+							//  打包apk时.gz打不进去，只能是解压后的数据
+							gzip: false
+						});
+						await worker.load();
+						await worker.loadLanguage('eng');
+						await worker.initialize('eng', 1);
+						await worker.setParameters({
+							tessedit_pageseg_mode: 6,
+						});
+					}catch(e){
+						ownerInstance.callMethod('getProcess', e)
+					}
+				}
+			},
+			receiveImg:async function(newValue, oldValue, ownerInstance, instance) {
+				if (newValue === '' || worker == undefined) {
+					return
+				}
+				try{
+					const { data: { text } } = await worker.recognize(newValue);
+					ownerInstance.callMethod('setText', text)
+				}catch(e){
+					ownerInstance.callMethod('getProcess', e)
+				}
+				
+			}
+		}
+	}
+</script>
+<style>
+	.page {
+		padding: 40rpx;
+	}
+	.tip-content {
+		text-align: center;
+		margin-top: 10rpx;
+		font-size: 40rpx;
+		font-weight: 700;
+	}
+</style>
